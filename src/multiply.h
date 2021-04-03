b019dbb326ccf6a24faa40ea47e8261497c0dda3
Subhajit Sahu <wolfram77@gmail.com>
2021-04-03 21:11:04 +0530

    :microbe: w/ multiplyAbsKernel()

--- src/multiply.h
+++ src/multiply.h
@@ -1,35 +1,65 @@
 #pragma once
-#include <vector>
 #include <unordered_map>
 #include <algorithm>
+#include <cmath>
 #include <omp.h>
 #include "_cuda.h"
 
-using std::vector;
 using std::unordered_map;
 using std::max;
+using std::abs;
 
 
 
+
+// MULTIPLY-VALUE
+// --------------
 
 template <class T>
-void multiply(T *x, int N, T v) {
+void multiplyValue(T *a, int N, T v) {
   for (int i=0; i<N; i++)
-    x[i] *= v;
+    a[i] *= v;
 }
 
-template <class T>
-void multiply(vector<T>& x, T v) {
-  multiply(x.data(), x.size(), v);
+template <class C, class T>
+void multiplyValue(C& a, T v) {
+  multiply(a.data(), a.size(), v);
 }
 
 template <class K, class T>
-void multiply(unordered_map<K, T>& x, T v) {
-  for (auto& p : x) p.second *= v;
+void multiplyValue(unordered_map<K, T>& a, T v) {
+  for (auto& p : a)
+    p.second *= v;
 }
 
 
 
+
+// MULTIPLY-VALUE-AT
+// -----------------
+
+template <class T, class I>
+void multiplyValueAt(T *a, I&& is , T v) {
+  for (int i : is)
+    a[i] *= v;
+}
+
+template <class C, class I, class T>
+void multiplyValueAt(C& a, I&& is, T v) {
+  multiplyValueAt(a.data(), is, v);
+}
+
+template <class K, class T, class I>
+void multiplyValueAt(unordered_map<K, T>& a, I&& ks, T v) {
+  for (auto&& k : ks)
+    a[k] *= v;
+}
+
+
+
+
+// MULTIPLY
+// --------
 
 template <class T>
 void multiply(T *a, T *x, T *y, int N) {
@@ -37,72 +67,81 @@ void multiply(T *a, T *x, T *y, int N) {
     a[i] = x[i] * y[i];
 }
 
-template <class T>
-void multiply(vector<T>& a, vector<T>& x, vector<T>& y) {
-  multiply(a.data(), x.data(), y.data(), a.size());
+template <class C, class T>
+void multiply(C& a, C&& x, C&& y) {
+  multiply(a.data(), x.data(), y.data(), x.size());
 }
 
 template <class K, class T>
-void multiply(unordered_map<K, T>& a, unordered_map<K, T>& x, unordered_map<K, T>& y) {
-  for (auto& p : x)
+void multiply(unordered_map<K, T>& a, unordered_map<K, T>&& x, unordered_map<K, T>&& y) {
+  for (auto&& p : x)
     a[p.first] = x[p.first] * y[p.first];
 }
 
 
 
 
-template <class T, class C>
-void multiplyAt(T *x, C&& is , T v) {
-  for (int i : is)
-    x[i] *= v;
-}
-
-template <class T, class C>
-void multiplyAt(vector<T>& x, C&& is, T v) {
-  multiplyAt(x.data(), is, v);
-}
-
-template <class K, class T, class C>
-void multiplyAt(unordered_map<K, T>& x, C&& ks, T v) {
-  for (auto&& k : ks)
-    x[k] *= v;
-}
-
-
-
+// MULTIPLY-ABS
+// ------------
 
 template <class T>
-void multiplyOmp(T *x, int N, T v) {
+void multiplyAbs(T *a, T *x, T *y, int N) {
+  for (int i=0; i<N; i++)
+    a[i] = abs(x[i] * y[i]);
+}
+
+template <class C, class T>
+void multiplyAbs(C& a, C&& x, C&& y) {
+  multiplyAbs(a.data(), x.data(), y.data(), x.size());
+}
+
+template <class K, class T>
+void multiplyAbs(unordered_map<K, T>& a, unordered_map<K, T>&& x, unordered_map<K, T>&& y) {
+  for (auto&& p : x)
+    a[p.first] = abs(x[p.first] * y[p.first]);
+}
+
+
+
+
+// MULTIPLY-VALUE (OMP)
+// --------------------
+
+template <class T>
+void multiplyValueOmp(T *a, int N, T v) {
   #pragma omp parallel for
   for (int i=0; i<N; i++)
-    x[i] *= v;
+    a[i] *= v;
 }
 
-template <class T>
-void multiplyOmp(vector<T>& x, T v) {
-  multiplyOmp(x.data(), x.size(), v);
+template <class C, class T>
+void multiplyOmp(C& a, T v) {
+  multiplyValueOmp(a.data(), a.size(), v);
 }
 
 
 
 
+// MULTIPLY-VALUE (CUDA)
+// ---------------------
+
 template <class T>
-__device__ void multiplyKernelLoop(T *a, int N, T v, int i, int DI) {
+__device__ void multiplyValueKernelLoop(T *a, int N, T v, int i, int DI) {
   for (; i<N; i+=DI)
     a[i] *= v;
 }
 
 
 template <class T>
-__global__ void multiplyKernel(T *a, int N, T v) {
+__global__ void multiplyValueKernel(T *a, int N, T v) {
   DEFINE(t, b, B, G);
 
-  multiplyKernelLoop(a, N, v, B*b+t, G*B);
+  multiplyValueKernelLoop(a, N, v, B*b+t, G*B);
 }
 
 
 template <class T>
-void multiplyCuda(T *a, int N, T v) {
+void multiplyValueCuda(T *a, int N, T v) {
   int B = BLOCK_DIM;
   int G = min(ceilDiv(N, B), GRID_DIM);
   size_t N1 = N * sizeof(T);
@@ -111,19 +150,22 @@ void multiplyCuda(T *a, int N, T v) {
   TRY( cudaMalloc(&aD, N1) );
   TRY( cudaMemcpy(aD, a, N1, cudaMemcpyHostToDevice) );
 
-  multiplyKernel<<<G, B>>>(aD, N, v);
+  multiplyValueKernel<<<G, B>>>(aD, N, v);
   TRY( cudaMemcpy(a, aD, N1, cudaMemcpyDeviceToHost) );
 
   TRY( cudaFree(aD) );
 }
 
-template <class T>
-void multiplyCuda(vector<T>& x, T v) {
-  multiplyCuda(x.data(), x.size(), v);
+template <class C, class T>
+void multiplyValueCuda(C& a, T v) {
+  multiplyValueCuda(a.data(), a.size(), v);
 }
 
 
 
+
+// MULTIPLY (CUDA)
+// ---------------
 
 template <class T>
 __device__ void multiplyKernelLoop(T *a, T *x, T *y, int N, int i, int DI) {
@@ -159,7 +201,52 @@ void multiplyCuda(T *a, T *x, T *y, int N, T v) {
   TRY( cudaFree(yD) );
 }
 
+template <class C, class T>
+void multiplyCuda(C& a, C&& x, C&& y) {
+  multiplyCuda(a.data(), x.data(), y.data(), x.size());
+}
+
+
+
+
+// MULTIPLY-ABS (CUDA)
+// -------------------
+
 template <class T>
-void multiplyCuda(vector<T>& a, vector<T>& x, vector<T> &y) {
-  multiplyCuda(a.data(), x.data(), y.data(), a.size());
+__device__ void multiplyAbsKernelLoop(T *a, T *x, T *y, int N, int i, int DI) {
+  for (; i<N; i+=DI)
+    a[i] = abs(x[i] * y[i]);
+}
+
+
+template <class T>
+__global__ void multiplyAbsKernel(T *a, T *x, T* y, int N) {
+  DEFINE(t, b, B, G);
+
+  multiplyAbsKernelLoop(a, x, y, N, B*b+t, G*B);
+}
+
+
+template <class T>
+void multiplyAbsCuda(T *a, T *x, T *y, int N, T v) {
+  int B = BLOCK_DIM;
+  int G = min(ceilDiv(N, B), GRID_DIM);
+  size_t N1 = N * sizeof(T);
+
+  T *xD, *yD;
+  TRY( cudaMalloc(&xD, N1) );
+  TRY( cudaMalloc(&yD, N1) );
+  TRY( cudaMemcpy(xD, x, N1, cudaMemcpyHostToDevice) );
+  TRY( cudaMemcpy(yD, y, N1, cudaMemcpyHostToDevice) );
+
+  multiplyKernel<<<G, B>>>(xD, xD, yD, N);
+  TRY( cudaMemcpy(a, xD, N1, cudaMemcpyDeviceToHost) );
+
+  TRY( cudaFree(xD) );
+  TRY( cudaFree(yD) );
+}
+
+template <class C, class T>
+void multiplyAbsCuda(C& a, C&& x, C&& y) {
+  multiplyAbsCuda(a.data(), x.data(), y.data(), x.size());
 }
