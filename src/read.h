3a771b5f72ac7174f67c4a224baa579c520da9e3
Subhajit Sahu <wolfram77@gmail.com>
2021-04-03 07:49:51 +0530

    :microbe: add comments to readMtx

--- src/read.h
+++ src/read.h
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include "DiGraph.h"
 
 using std::ios;
 using std::string;
@@ -13,29 +14,34 @@ using std::getline;
 
 
 template <class G>
-void readMtx(string pth, G& a) {
-  int r, c, sz;
-  string b;
+void readMtx(string& pth, G& a) {
   ifstream f(pth);
+  string b, ln;
 
+  // read all data
   f.seekg(0, ios::end);
   b.resize(f.tellg());
-
   f.seekg(0);
   f.read((char*) b.data(), b.size());
-
-  string ln;
   istringstream bs(b);
+
+  // read rows, cols, size
+  int r, c, sz;
   do { getline(bs, ln); }
   while (ln[0] == '%');
-
   istringstream ls(ln);
   ls >> r >> c >> sz;
 
+  // read edges (from, to)
   while (getline(bs, ln)) {
     int u, v;
     ls = istringstream(ln);
     if (!(ls >> u >> v)) break;
     a.addEdge(u, v);
   }
+}
+
+auto readMtx(string& pth) {
+  DiGraph<> a; readMtx(pth, a);
+  return a;
 }
