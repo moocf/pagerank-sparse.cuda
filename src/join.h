449d730159bfcc1722f8c9da34bcd1b5147232fa
Subhajit Sahu <wolfram77@gmail.com>
2021-04-07 12:04:15 +0530

    :microbe: w/ join

--- src/join.h
+++ src/join.h
@@ -6,8 +6,17 @@ using std::vector;
 
 
 
-template <class T, class C>
-auto joinFrom(vector<vector<T>>& xs, C&& is) {
+template <class T>
+auto join(vector<vector<T>>& xs) {
+  vector<T> a;
+  for (auto& x : xs)
+    a.insert(a.end(), x.begin(), x.end());
+  return a;
+}
+
+
+template <class T, class I>
+auto joinFrom(vector<vector<T>>& xs, I&& is) {
   vector<T> a;
   for (int i : is)
     a.insert(a.end(), xs[i].begin(), xs[i].end());
