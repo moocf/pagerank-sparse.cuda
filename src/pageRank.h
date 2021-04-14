dc41ed3e3f72aa7a30555c912c4f5ce536396ae3
Subhajit Sahu <wolfram77@gmail.com>
2021-04-14 22:00:14 +0530

    :microbe: enable call to pageRankSpecialKernel()

--- src/pageRank.h
+++ src/pageRank.h
@@ -397,7 +397,7 @@ T* pageRankCudaLoop(T* e, T *r0, T *eD, T *r0D, T *aD, T *cD, T *rD, T *fD, int 
     TRY( cudaMemcpy(r0, r0D, H1, cudaMemcpyDeviceToHost) );
     T c0 = (1-p)/N + p*sum(r0, G)/N;
     pageRankKernelWave(aD, rD, cD, vfromD, efromD, c0, _fSC, i, ns);
-    // if (fRI || fRC) pageRankSpecialKernel<<<G, B>>>(aD, rD, vfromD, vdataD, c0, p, i, n);
+    if (vrootD) pageRankSpecialKernel<<<G, B>>>(aD, rD, vrootD, vdistD, c0, p, i, n);
     absErrorKernel<<<G, B>>>(eD, rD, aD, N);
     TRY( cudaMemcpy(e, eD, G1, cudaMemcpyDeviceToHost) );
     T e1 = sum(e, G);
