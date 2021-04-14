dceb445f3430c4033807266efd0e17e8d7d321cd
Subhajit Sahu <wolfram77@gmail.com>
2021-04-14 14:52:50 +0530

    :microbe: fix inIdenticals()

--- src/identicals.h
+++ src/identicals.h
@@ -10,23 +10,23 @@ using std::vector;
 
 
 
-template <class G, class K>
-auto identicalSiblings(G& xt, vector<K>& vs, int i, set<K>& s) {
+template <class G, class K, class F>
+auto identicalSiblings(G& xt, vector<K>& vs, int i, set<K>& s, F fn) {
   vector<K> a;
-  K vi = vs[i];
+  auto vi = vs[i];
   setFrom(s, xt.edges(vi));
   for (int j=i+1, J=vs.size(); j<J; j++) {
     auto vj = vs[j];
     if (s.size() != xt.degree(vj)) continue;
-    if (contains(s, xt.edges(vj))) a.push_back(vj);
+    if (contains(s, xt.edges(vj)) && fn(vj)) a.push_back(vj);
   }
-  if (a.size() > 0) a.insert(0, vi);
+  if (a.size() > 0) a.insert(a.begin(), vi);
   return a;
 }
 
 
-template <class G, class H>
-auto inIdenticals(G& x, H& xt) {
+template <class G, class H, class F>
+auto inIdenticals(G& x, H& xt, F fn) {
   using K = typename G::TKey;
   vector<vector<K>> a;
   vector<K> vs;
@@ -39,9 +39,15 @@ auto inIdenticals(G& x, H& xt) {
     for (int i=0, I=vs.size(); i<I; i++) {
       if (vis[vs[i]]) continue;
 
-      auto b = identicalSiblings(xt, vs, i, s);
+      auto b = identicalSiblings(xt, vs, i, s, fn);
       if (b.size() > 0) a.push_back(b);
       vis[vs[i]] = true;
     }
   }
+  return a;
+}
+
+template <class G, class H>
+auto inIdenticals(G& x, H& xt) {
+  return inIdenticals(x, xt, [](auto u) { return true; });
 }
