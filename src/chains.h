ce2b023e2b2de91e031de323a1beb689c2ae7cfd
Subhajit Sahu <wolfram77@gmail.com>
2021-04-16 00:44:47 +0530

    :microbe: fix chains()

--- src/chains.h
+++ src/chains.h
@@ -9,16 +9,29 @@ using std::vector;
 
 template <class G, class H, class K>
 auto chainRoot(G& x, H& xt, K u) {
-  while (x.degree(u) == 1 && xt.degree(u) == 1)
-    u = edge(xt, u);
+  K u0 = u;
+  while (1) {
+    K v = edge(xt, u);
+    if (v == u0 || x.degree(v) != 1 || xt.degree(v) != 1) break;
+    u = v;
+  }
   return u;
 }
 
 
-template <class G, class H, class K, class C>
-auto chainAt(G& x, H& xt, K u, C& vis) {
+template <class G, class H, class C, class K>
+int hasChain(G& x, H& xt, C& vis, K u) {
+  int n = 0;
+  for (; n<2 && !vis[u] && x.degree(u) == 1 && xt.degree(u) == 1; n++)
+    u = edge(x, u);
+  return n >= 2;
+}
+
+
+template <class G, class H, class C, class K>
+auto chainAt(G& x, H& xt, C& vis, K u) {
   vector<K> a;
-  while (x.degree(u) == 1 && xt.degree(u) == 1) {
+  while (!vis[u] && x.degree(u) == 1 && xt.degree(u) == 1) {
     vis[u] = true;
     a.push_back(u);
     u = edge(x, u);
@@ -33,14 +46,11 @@ auto chains(G& x, H& xt) {
   vector<vector<K>> a;
   auto vis = x.vertexContainer(bool());
   for (auto u : x.vertices()) {
+    if (vis[u] || x.degree(u) != 1 || xt.degree(u) != 1) continue;
     u = chainRoot(x, xt, u);
-    if (vis[u]) continue;
-    vis[u] = true;
-    for (auto v : x.edges(u)) {
-      auto b = chainAt(x, xt, v, vis);
-      if (b.size() < 2) continue;
-      a.push_back(b);
-    }
+    if (!hasChain(x, xt, vis, u)) continue;
+    auto b = chainAt(x, xt, vis, u);
+    a.push_back(b);
   }
   return a;
 }
