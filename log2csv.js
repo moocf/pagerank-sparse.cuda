21dfbc5a39c81d2f174d490edfdfd5aec4229404
Subhajit Sahu <wolfram77@gmail.com>
2021-05-02 19:10:46 +0530

    :skunk: handle new properties in log2csv

--- log2csv.js
+++ log2csv.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 const os = require('os');
 
-const RGRAPH  = /Loading\s+graph\s+(\S+\/([^\/]*?).mtx)\s+\.\.\./;
-const RORDER =  /order:\s*(\d+)\s+size:\s*(\d+)/;
-const RRESULT = /\[(\S*)\s+ms\]\s+\[(\S*)\]\s+(\S*)(?:\s+\{(\S*)\}\s+\{(.*)\})?/;
+const RGRAPH  = /^Loading\s+graph\s+(\S+\/([^\/]*?).mtx)\s+\.\.\./m;
+const RORDER  = /^order:\s*(\d+)\s+size:\s*(\d+)\s*\{\}$/m;
+const RCROSSE = /^order:\s*(\d+)\s+size:\s*(\d+)\s*\{\}\s*\(crossEdges\)/m;
+const RCROSSF = /^order:\s*(\d+)\s+size:\s*(\d+)\s*\{\}\s*\(crossEdgesFree\)/m;
+const RCOMPS  = /^components:\s*(\d+)\s+largeComponents:\s*(\d+)/m;
+const RPROPS  = /^deadEnds:\s*(\d+)\s+inIdenticals:\s*(\d+)\s+chains:\s*(\d+)/m;
+const RRESULT = /\[(\S*)\s+ms\]\s+\[(\S*)\]\s+(\S*)(?:\s+\{(\S*)\}\s+\{(.*)\})?/m;
 
 
 
@@ -54,15 +58,40 @@ function parseLog(pth) {
       g.order = parseFloat(order);
       g.size  = parseFloat(size);
     }
+    else if (RCROSSE.test(l)) {
+      var [,, crossEdges] = l.match(RCROSSE);
+      g.crossEdges = parseFloat(crossEdges);
+    }
+    else if (RCROSSF.test(l)) {
+      var [,, crossEdgesFree] = l.match(RCROSSF);
+      g.crossEdgesFree = parseFloat(crossEdgesFree);
+    }
+    else if (RCOMPS.test(l)) {
+      var [, components, largeComponents] = l.match(RCOMPS);
+      g.components      = parseFloat(components);
+      g.largeComponents = parseFloat(largeComponents);
+    }
+    else if (RPROPS.test(l)) {
+      var [, deadEnds, inIdenticals, chains] = l.match(RPROPS);
+      g.deadEnds     = parseFloat(deadEnds);
+      g.inIdenticals = parseFloat(inIdenticals);
+      g.chains       = parseFloat(chains);
+    }
     else if (RRESULT.test(l)) {
       var [, time, error, fn, mode, flags] = l.match(RRESULT);
-      var r      = {};
-      r.graph    = g.name;
-      r.vertices = g.order;
-      r.edges    = g.size;
-      r.config   = resultConfig(fn, mode, flags);
-      r.error    = parseFloat(error);
-      r.time     = parseFloat(time);
+      var r             = {};
+      r.graph           = g.name;
+      r.vertices        = g.order;
+      r.edges           = g.size;
+      r.crossEdges      = g.crossEdges;
+      r.components      = g.components;
+      r.largeComponents = g.largeComponents;
+      r.deadEnds        = g.deadEnds;
+      r.inIdenticals    = g.inIdenticals;
+      r.chains          = g.chains;
+      r.config          = resultConfig(fn, mode, flags);
+      r.error           = parseFloat(error);
+      r.time            = parseFloat(time);
       if (!a.has(r.graph)) a.set(r.graph, []);
       a.get(r.graph).push(r);
     }
@@ -78,7 +107,7 @@ function postProcess(m) {
     for (var r of rs) {
       r.time_nvgraph = time_nvgraph;
       r.speedup      = time_nvgraph/r.time;
-      r.speedupf     = r.speedup * (r.vertices + r.edges);
+      r.speedupve    = r.speedup * (r.vertices + r.edges);
     }
     rs.sort((r, s) => r.time - s.time);
     a.push(...rs);
